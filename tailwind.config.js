77f9c152e1f13735b2a2301c0ea2e9a8430052c6
Sidharth Ahuja <siddhathahuja73@gmail.com>
2024-11-18 03:56:49 +0530

    added tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        customGray: '#a5a5a5',
+        customBlack: "#252525",
+      },
+    },
+  },
   plugins: [],
 };
